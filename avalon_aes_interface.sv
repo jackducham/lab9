4b1b3dcbe85024174ff700ff95fcb6951f08d64e
adugad <annika.dugad@gmail.com>
2018-10-28 16:39:13 -0500

    aes interface

--- avalon_aes_interface.sv
+++ avalon_aes_interface.sv
@@ -38,5 +38,47 @@ module avalon_aes_interface (
 	output logic [31:0] EXPORT_DATA		// Exported Conduit Signal to LEDs
 );
 
+logic [31:0] in,out;
+
+reg_file AES_REG_FILE(.Clk(CLK),.Reset(RESET),.LD_REG(AVL_CS && AVL_WRITE),.ADDR(AVL_ADDR),
+							 .INPUT(in),.out_en_first(EXPORT_DATA[31:16]),.out_en_last(EXPORT_DATA[15:0]),.OUTPUT(out));
+ 
+always_ff @ (posedge CLK)
+begin
+    if(RESET)
+    begin
+			AVL_READDATA <= {32{1'b0}};
+			in <= {32{1'b0}};
+    end
+	 
+    else
+    begin
+	 
+		if(AVL_CS && AVL_READ)
+		begin
+			AVL_READDATA <= out;
+		end
+		
+		else if(AVL_CS && AVL_WRITE)
+		begin
+			case(AVL_BYTE_EN)
+				4'b1111: in <= AVL_WRITEDATA;
+				4'b1100: in <= {AVL_WRITEDATA[31:16],16'hz};
+				4'b0011: in <= {16'hz,AVL_WRITEDATA[15:0]};
+				4'b1000: in <= {AVL_WRITEDATA[31:24],24'hz};
+				4'b0100: in <= {8'hz,AVL_WRITEDATA[23:16],16'hz};
+				4'b0010: in <= {16'hz,AVL_WRITEDATA[15:8],8'hz};
+				4'b0001: in <= {24'hz,AVL_WRITEDATA[7:0]};
+				default: in <= 32'hz;
+			endcase
+		end
+		
+		else
+		begin
+			AVL_READDATA <= {32{1'b0}};
+		end
+		
+    end
+end
 
 endmodule
